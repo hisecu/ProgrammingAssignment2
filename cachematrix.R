d162c04f20a90ea1b28eb6bfaf9947f9b9a093a4
Carol Fenijn <github@nomeka.org>
2015-02-20 14:24:22 +0100

    Some more comments.

--- cachematrix.R
+++ cachematrix.R
@@ -3,21 +3,40 @@
 ##
 ## Course URI:
 ##   https://class.coursera.org/rprog-011/
+##
 ## Assignment URI:
 ##   https://class.coursera.org/rprog-011/human_grading/view/courses/973492/assessments/3/submissions
 ##
 ## C Fenijn, February 2015.
 #
 
-## Write a short comment describing this function
+##
+## makeCacheMatrix is a function that will create the inverse matrix object
+## of a given input matrix object. 
+##
+## This inverse matrix object will be cached, so that it can be retrieved
+## efficiently without the need of computing it over and over again, which
+## would be very costly. 
+##
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+##
+## cacheSolve is a function that will return the inverse matrix
+## of a given input matrix.
+##
+## If the inverse matrix has been computed before, a cached
+## version of it will be returned.
+##
+## If the inverse matrix has not been computed before, it will
+## be computed on the fly and it will be added to the cache.
+##
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+
