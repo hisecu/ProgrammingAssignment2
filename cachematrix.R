77b58b06b088e981c0066b9e4cde7779152cfd72
Carol Fenijn <Carol Fenijn github@nomeka.org>
2015-02-21 17:15:45 +0100

    Some content, but I am still working on this, I hope this is getting close...

--- cachematrix.R
+++ cachematrix.R
@@ -7,10 +7,11 @@
 ## Assignment URI:
 ##   https://class.coursera.org/rprog-011/human_grading/view/courses/973492/assessments/3/submissions
 ##
-## Note that we strive for a relatively simple approach. If necessary, it can be made more sophisticated
+## Note that we strive for a relatively simple approach.
+## If necessary, it can be made more sophisticated
 ## at a later point.
 ##
-## C Fenijn, February 2015.
+## Carol Fenijn, February 2015.
 #
 
 ##
@@ -21,9 +22,61 @@
 ## efficiently without the need of computing it over and over again, which
 ## would be very costly. 
 ##
+## For this to work, we set values of variables in the more global
+## parent name space / environment in functions. Normally, we would try
+## to avoid techniques like these, but we are asked to do this as part
+## of this assignment. 
+##
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        #
+        # Initialize inversematrix
+        #
+        inversematrix <- NULL
+
+        #
+        # Define the set() function. 
+        #
+        set <- function(y) {
+                x <<- y
+                inversematrix <<- NULL
+        }
+
+        #
+        # Define the get() function. 
+        #
+        get <- function() {
+                x
+        }
+
+        #
+        # Define the get() function. 
+        #
+        setinversematrix <- function(inversematrix) {
+                # Note that inversematrix is set more globally
+                # here, so in the parent name space / environment
+                inversematrix <<- cacheSolve(x)
+        }
+
+
+        #
+        # Define the get() function. 
+        #
+        getinversematrix <- function() {
+                inversematrix
+        }
+
+        #
+        # The following list of functions is returned, so the functions
+        # are accessible as makeCacheMatrix$get(), makeCacheMatrix$invertmatrix() etc.
+        #
+        list(
+             set = set,
+             get = get,
+             invertmatrix = invertmatrix,
+             getinversematrix = getinversematrix
+            )
 }
 
 
@@ -35,11 +88,54 @@ makeCacheMatrix <- function(x = matrix()) {
 ## version of it will be returned.
 ##
 ## If the inverse matrix has not been computed before, it will
-## be computed on the fly and it will be added to the cache.
+## be computed on the fly by cacheSolve() and it will be added
+## to the cache.
 ##
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ##
+    ## Return a matrix that is the inverse of 'x'
+    ##
+    #
+    # I find input_m more readable than x, as it is the input matrix:
+    #
+    retrieved_m <- x$getinversematrix()
+
+    #
+    # calculate the inverse of input_m:
+    #
+    if(is.null(retrieved_m)) {
+            inverse_m <- solve(retrieved_m)
+    }
+    else
+    {
+            message("getting cached data")
+            inverse_m <- retrieved_m
+    }
+      
+    #
+    # Return the inverse matrix of input_m:
+    #
+    return(inverse_m)
 }
 
-
+#
+#sample_m <- rbind(c(00000001, 00000002), c(00000003, 00000004))
+#
+#class(sample_m)
+#
+#sample_m
+#
+#inv_sample_m <- cacheSolve(sample_m)
+#
+#inv_sample_m
+#
+#sample2_m <- matrix( c(1, 1, 3, 1), nrow=2, ncol=2)
+#
+#class(sample2_m)
+#
+#inv_sample2_m <- cacheSolve(sample2_m)
+#
+#inv_sample2_m
+#
+#
