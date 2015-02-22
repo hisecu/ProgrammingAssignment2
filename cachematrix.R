2350af0610b2fc0bf4a9b3b524a23fdb4bbe6024
Carol Fenijn <Carol Fenijn github@nomeka.org>
2015-02-22 19:27:02 +0100

    The previous version did not work, this is a working version, I think.

--- cachematrix.R
+++ cachematrix.R
@@ -12,71 +12,44 @@
 ## at a later point.
 ##
 ## Carol Fenijn, February 2015.
-#
 
 ##
-## makeCacheMatrix is a function that will create the inverse matrix object
-## of a given input matrix object. 
+## makeCacheMatrix is a function that will make various other functions available.
 ##
-## This inverse matrix object will be cached, so that it can be retrieved
-## efficiently without the need of computing it over and over again, which
-## would be very costly. 
+## An inverse matrix object will be cached with the setmatrix() function, so that
+## it can be retrieved efficiently without the need of computing it over and over
+## again, which would be very costly. 
 ##
 ## For this to work, we set values of variables in the more global
-## parent name space / environment in functions. Normally, we would try
-## to avoid techniques like these, but we are asked to do this as part
-## of this assignment. 
+## parent name space / environment in functions.
 ##
-
 makeCacheMatrix <- function(x = matrix()) {
 
-        #
-        # Initialize inversematrix
-        #
-        inversematrix <- NULL
+        m <- NULL    # initialization of m
 
         #
-        # Define the set() function. 
+        # Define the set() function. This will set a value to a variable in 
+        # the more global name space / environment
         #
         set <- function(y) {
                 x <<- y
-                inversematrix <<- NULL
+                m <<- NULL
         }
 
         #
-        # Define the get() function. 
+        # Define the get() function. It will just return a value 
         #
-        get <- function() {
-                x
-        }
+        get <- function() x
 
-        #
-        # Define the get() function. 
-        #
-        setinversematrix <- function(inversematrix) {
-                # Note that inversematrix is set more globally
-                # here, so in the parent name space / environment
-                inversematrix <<- cacheSolve(x)
-        }
+        # Note that inversematrix is set more globally
+        # here, so in the parent name space / environment
+        setmatrix <- function(inv_matrix) m <<- inv_matrix
 
+        getmatrix <- function() m
 
-        #
-        # Define the get() function. 
-        #
-        getinversematrix <- function() {
-                inversematrix
-        }
-
-        #
-        # The following list of functions is returned, so the functions
-        # are accessible as makeCacheMatrix$get(), makeCacheMatrix$invertmatrix() etc.
-        #
-        list(
-             set = set,
-             get = get,
-             invertmatrix = invertmatrix,
-             getinversematrix = getinversematrix
-            )
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
 
 
@@ -91,51 +64,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## be computed on the fly by cacheSolve() and it will be added
 ## to the cache.
 ##
-
 cacheSolve <- function(x, ...) {
-    ##
-    ## Return a matrix that is the inverse of 'x'
-    ##
-    #
-    # I find input_m more readable than x, as it is the input matrix:
-    #
-    retrieved_m <- x$getinversematrix()
-
-    #
-    # calculate the inverse of input_m:
-    #
-    if(is.null(retrieved_m)) {
-            inverse_m <- solve(retrieved_m)
-    }
-    else
-    {
-            message("getting cached data")
-            inverse_m <- retrieved_m
-    }
-      
-    #
-    # Return the inverse matrix of input_m:
-    #
-    return(inverse_m)
+        m <- x$getmatrix()
+        if(is.null(m)) {
+                data <- x$get()
+                m <- solve(data, ...)
+                x$setmatrix(m)
+        }
+        else
+        {
+                message("getting cached data")
+        }
+        return(m)
 }
 
-#
-#sample_m <- rbind(c(00000001, 00000002), c(00000003, 00000004))
-#
-#class(sample_m)
-#
-#sample_m
-#
-#inv_sample_m <- cacheSolve(sample_m)
-#
-#inv_sample_m
-#
-#sample2_m <- matrix( c(1, 1, 3, 1), nrow=2, ncol=2)
-#
-#class(sample2_m)
-#
-#inv_sample2_m <- cacheSolve(sample2_m)
-#
-#inv_sample2_m
-#
-#
